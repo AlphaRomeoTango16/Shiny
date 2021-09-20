baff30a9a9e906682947e892e4cb12da8422a934
Arthur Della Faille <arthur16.dellafaille@gmail.com>
2021-09-20 13:08:51 +0200

    test

---
+++ src/components/Footer/index.test.js
@@ -0,0 +1,18 @@
+import Footer from './index'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { ThemeProvider } from '../../utils/context/index'
+
+describe('Footer', () => {
+    test('Change theme', async () => {
+        render(
+            <ThemeProvider>
+                <Footer />
+            </ThemeProvider>
+        )
+        const nightModeButton = screen.getByRole('button')
+        expect(nightModeButton.textContent).toBe('Changer de mode : 🌙')
+        fireEvent.click(nightModeButton)
+        expect(nightModeButton.textContent).toBe('Changer de mode : ☀️')
+    })
+})
+
--- src/pages/Results/index.jsx
+++ src/pages/Results/index.jsx
@@ -63,6 +63,24 @@ function formatFetchParams(answers) {
   }, '')
 }
 
+export function formatQueryParams(answers) {
+  const answerNumbers = Object.keys(answers)
+
+  return answerNumbers.reduce((previousParams, answerNumber, index) => {
+    const isFirstParam = index === 0
+    const separator = isFirstParam ? '' : '&'
+    return `${previousParams}${separator}a${answerNumber}=${answers[answerNumber]}`
+  }, '')
+}
+
+export function formatJobList(title, listLength, index) {
+  if (index === listLength -1) {
+    return title
+  } else {
+    return `${title},`
+  }
+}
+
 function Results() {
   const { theme } = useContext(ThemeContext)
   const { answers } = useContext(SurveyContext)
@@ -92,8 +110,7 @@ function Results() {
               key={`result-title-${index}-${result.title}`}
               theme={theme}
             >
-              {result.title}
-              {index === resultsData.length - 1 ? '' : ','}
+              {formatJobList(result.title, resultsData.listLength, index)}
             </JobTitle>
           ))}
       </ResultsTitle>
